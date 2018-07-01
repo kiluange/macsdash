c2be124f202d646f62fe18ebe252531fb1e52427
kiluange <daniel.kiluange@gmail.com>
2018-07-01 20:52:09 -0300

    first commit

--- src/minicards.js
+++ src/minicards.js
@@ -7,16 +7,16 @@ export default class Minicards extends Component{
         <div className='container'>
           <div className='row'>
             <div className='col-lg-3 col-md-6 col-xm-12'>
-              <Minicard color='#9C27B0' text='Domicílios' icon='home' total={50}/>
+              <Minicard color='#9C27B0' text='Domicílios' icon='home' total={2}/>
             </div>
             <div className='col-lg-3 col-md-6 col-xm-12'>
-              <Minicard color='#673AB7' text='Famílias' icon='users' total={50}/>
+              <Minicard color='#673AB7' text='Famílias' icon='users' total={3}/>
             </div>
             <div className='col-lg-3 col-md-6 col-xm-12'>
-              <Minicard color='#FF7043' text='Indivíduos' icon='user' total={50}/>
+              <Minicard color='#FF7043' text='Indivíduos' icon='user' total={7}/>
             </div>
             <div className='col-lg-3 col-md-6 col-xm-12'>
-              <Minicard color='#2196F3' text='Visitas' icon='hand-holding-heart' total={50}/>
+              <Minicard color='#2196F3' text='Visitas' icon='hand-holding-heart' total={5}/>
             </div>
           </div>
         </div>
